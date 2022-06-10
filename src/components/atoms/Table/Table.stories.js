3e5d67085d61292ed4ea9f727df318cf944ae344
Chigoziem Nwaiwu <princegoziem@gmail.com>
2022-06-10 09:56:39 +0100

    feat: add width functionality to table

--- src/components/atoms/Table/Table.stories.js
+++ src/components/atoms/Table/Table.stories.js
@@ -1,30 +1,18 @@
 import Table from "./index.vue";
-<<<<<<< HEAD
 import Button from "../Button/index.vue";
 import TableEmptyState from "../../molecules/TableEmptyState.vue";
-=======
->>>>>>> feat: basic table setup
 
 export default { title: "Atoms/Table", component: Table };
 
 export const TableComponent = (args) => ({
-<<<<<<< HEAD
   components: { Table, Button, TableEmptyState },
-=======
-  components: { Table },
->>>>>>> feat: basic table setup
   setup() {
     const columns = [
       { label: "Name", prop: "name" },
       { label: "Title", prop: "title" },
       { label: "Email", prop: "email" },
-<<<<<<< HEAD
       { label: "Role", prop: "role", width: 100 },
       { label: "Edit", prop: "edit", hideLabel: true, width: 100 },
-=======
-      { label: "Role", prop: "role" },
-      { label: "Edit", prop: "edit" },
->>>>>>> feat: basic table setup
     ];
 
     const tableData = [
@@ -42,7 +30,6 @@ export const TableComponent = (args) => ({
       },
     ];
 
-<<<<<<< HEAD
     const logger = () => console.log("logger===>>>");
 
     return { args, columns, tableData, logger };
@@ -53,14 +40,7 @@ export const TableComponent = (args) => ({
     </template>
 
     <template v-slot:empty>
-      <TableEmptyState @btn:clicked="logger" btnLabel="Hello Button" />
-=======
-    return { args, columns, tableData };
-  },
-  template: `<Table :data="tableData" v-bind="args" :columns="columns">
-  <template v-slot:edit="{row}">
-      <div>Action New {{ row.role }}</div>
->>>>>>> feat: basic table setup
+      <TableEmptyState @btn:clicked="logger" btnLabel="Add Beneficiary" />
     </template>
   </Table>`,
 });
