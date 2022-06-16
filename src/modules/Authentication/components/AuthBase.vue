8a2b9f6292c1bc28cb8ff6020fa41a7af68207c0
Chigoziem Nwaiwu <princegoziem@gmail.com>
2022-06-16 07:23:50 +0100

    feat: implement sign up

--- src/modules/Authentication/components/AuthBase.vue
+++ src/modules/Authentication/components/AuthBase.vue
@@ -1,32 +1,31 @@
 <template>
-  <div class="relative flex h-full w-full">
+  <div class="flex h-screen w-full">
     <div
-      class="hidden h-full w-[644px] bg-[#F1FAF9] py-3.5 pl-[123px] pr-10 lg:block"
+      class="hidden w-[644px] flex-col space-y-10 bg-[#F1FAF9] px-12 py-7 lg:flex"
     >
-      <span
-        class="absolute inset-0 top-7.5 left-[123px] block text-xl font-bold text-blue"
-        >Logo</span
-      >
-      <main class="grid h-full place-items-center">
-        <div>
-          <slot name="info">
-            <Icon name="auth-base" />
-            <div class="mt-20">
+      <span class="left-12 text-xl font-bold text-blue"> Logo </span>
+
+      <div class="flex flex-1 flex-col items-center justify-center space-y-20">
+        <slot name="info">
+          <Icon name="auth-base" />
+
+          <div class="w-full">
+            <div class="mx-auto w-3/4">
               <h1
                 class="text-center text-[32px] font-bold leading-[35px] text-black1"
               >
-                Dont Ever Miss A Payment
+                Don't Ever Miss A Payment
               </h1>
               <h2 class="mt-4 text-center text-sm leading-[20px] text-black1">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit ut
                 aliquam, purus sit amet luctus venenatis
               </h2>
             </div>
-          </slot>
-        </div>
-      </main>
+          </div>
+        </slot>
+      </div>
     </div>
-    <div class="w-4/6">
+    <div class="flex flex-1 flex-col py-7 px-10 lg:px-12">
       <slot name="content" />
     </div>
   </div>
--- src/modules/Authentication/views/Login.vue
+++ src/modules/Authentication/views/Login.vue
@@ -1,9 +1,84 @@
 <template>
   <AuthBase>
-    <template #content></template>
+    <template #content>
+      <div class="flex flex-1 flex-col space-y-15 py-16">
+        <!-- <div class="text-right">
+          Don’t have an account?
+          <Button label="Sign up" ghost class="text-blue" />
+        </div> -->
+        <div class="flex justify-center">
+          <Button
+            label="Individual"
+            @click="accountType = 'individual'"
+            size="small"
+            type="light"
+            :ghost="accountType !== 'individual'"
+            class="px-7 py-3"
+          />
+          <Button
+            label="Corporate"
+            @click="accountType = 'corporate'"
+            size="small"
+            type="light"
+            :ghost="accountType !== 'corporate'"
+            class="px-7 py-3"
+          />
+        </div>
+        <div class="mx-auto w-3/4 flex-1 space-y-10 overflow-auto">
+          <div class="space-y-1">
+            <div class="text-2xl font-semibold">Sign Up to SoftPay</div>
+            <div>
+              Provide details that match with a valid business document.
+            </div>
+          </div>
+          <div class="space-y-3">
+            <div class="grid grid-cols-2 gap-5">
+              <TextField label="First Name" placeholderText="E.g Segun" />
+              <TextField label="last Name" placeholderText="E.g Doe" />
+            </div>
+            <TextField label="Email" placeholderText="E.g name@domain.com" />
+            <TextField
+              label="Business Name"
+              placeholderText="E.g SoftPay Limited"
+            />
+            <div>
+              <label class="text-xs text-gray1"
+                >Business Type
+                <SelectField
+                  :options="BUSINESS_OPTIONS"
+                  placeholderText="E.g name@domain.com"
+                />
+              </label>
+            </div>
+            <TextField
+              type="tel"
+              label="Business Name"
+              placeholderText="E.g SoftPay Limited"
+            />
+          </div>
+        </div>
+      </div>
+    </template>
   </AuthBase>
 </template>
 
 <script setup>
 import AuthBase from "../components/AuthBase.vue";
+import Button from "../../../components/atoms/Button.vue";
+import TextField from "../../../components/atoms/TextField.vue";
+import SelectField from "../../../components/atoms/SelectField.vue";
+import { ref, watch } from "vue";
+
+const accountType = ref("individual");
+
+const BUSINESS_OPTIONS = [
+  { label: "Agriculture", value: "agriculture" },
+  { label: "Art & Culture", value: "art_culture" },
+  { label: "Cloud Management", value: "cloud_management" },
+  { label: "Content Creator", value: "content_creator" },
+];
+
+watch(accountType, (type) => {
+  console.log(type);
+});
 </script>
