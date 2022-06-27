2e13ceeb99e3076abe5708f1342a15c13cda6182
Chigoziem Nwaiwu <princegoziem@gmail.com>
2022-06-27 07:42:15 +0100

    chore: added table

--- src/components/atoms/Pagination.vue
+++ src/components/atoms/Pagination.vue
@@ -1,31 +1,64 @@
 <template>
-  <div class="flex justify-between text-sm">
+  <div class="flex justify-between px-10 py-2 text-sm">
     <div class="flex items-center space-x-10">
       <div class="flex items-center space-x-5">
-        <Select :options="OPTIONS" />
+        <el-select v-model="pageNum" class="sp-pagination-select">
+          <el-option
+            v-for="item in OPTIONS"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          />
+        </el-select>
         <div>of 10</div>
       </div>
       <div
-        class="flex h-full items-center justify-center divide-x divide-gray6 rounded-lg border border-gray6"
+        class="flex h-full items-center justify-center divide-x divide-gray6 overflow-hidden rounded-lg border border-gray6"
       >
-        <Button icon="rightArrow" class="h-full rotate-[180deg] px-4" ghost />
-        <Button icon="rightArrow" class="h-full px-4" ghost disabled />
+        <Button
+          icon="arrowLeft"
+          class="h-full px-4 disabled:cursor-not-allowed disabled:bg-gray5"
+          ghost
+        />
+        <Button
+          icon="arrowRight"
+          class="h-full px-4 disabled:cursor-not-allowed disabled:bg-gray5"
+          ghost
+          disabled
+        />
       </div>
     </div>
     <div class="flex items-center space-x-5">
       <div>Entries per page</div>
-      <Select :options="OPTIONS" />
+      <el-select v-model="perPage" class="sp-pagination-select">
+        <el-option
+          v-for="item in OPTIONS"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value"
+        />
+      </el-select>
     </div>
   </div>
 </template>
 
 <script setup>
-import Select from "./SelectField.vue";
+import { ref } from "vue";
 import Button from "./Button.vue";
 
+const pageNum = ref(1);
+const perPage = ref(1);
 const OPTIONS = new Array(124)
   .fill()
   .map((_, i) => ({ label: i + 1, value: i + 1 }));
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss">
+.sp-pagination-select.el-select {
+  & .el-input__wrapper {
+    & .el-input__inner {
+      @apply w-6;
+    }
+  }
+}
+</style>
